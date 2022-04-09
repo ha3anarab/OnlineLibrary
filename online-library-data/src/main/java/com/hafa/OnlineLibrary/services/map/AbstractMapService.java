b47e3767616dd5a48a9d2596010265e9414e6353
Ha3aN <Ha3aN.arab@gmail.com>
2022-04-09 20:16:37 +0430

    add Map implementation of customer service

---
+++ online-library-data/src/main/java/com/hafa/OnlineLibrary/services/map/AbstractMapService.java
@@ -0,0 +1,28 @@
+package com.hafa.OnlineLibrary.services.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+}
---
+++ online-library-data/src/main/java/com/hafa/OnlineLibrary/services/map/CustomerServiceMap.java
@@ -0,0 +1,40 @@
+package com.hafa.OnlineLibrary.services.map;
+
+import com.hafa.OnlineLibrary.model.Customer;
+import com.hafa.OnlineLibrary.services.CustomerService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomerServiceMap extends AbstractMapService<Customer, Long> implements CustomerService {
+    @Override
+    public Set<Customer> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return super.save(customer.getId(), customer);
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Customer findByLastName(String lastName) {
+        return null;
+    }
+}
