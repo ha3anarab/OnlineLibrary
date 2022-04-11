4a1f1eabd805ddd872cab09b1c620120961e1ed1
Ha3aN <Ha3aN.arab@gmail.com>
2022-04-11 17:44:19 +0430

    remove Autowired

--- online-library-web/src/main/java/com/hafa/onlineLibrary/Controllers/CustomerController.java
+++ online-library-web/src/main/java/com/hafa/onlineLibrary/Controllers/CustomerController.java
@@ -1,7 +1,6 @@
 package com.hafa.onlineLibrary.Controllers;
 
 import com.hafa.OnlineLibrary.services.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customers")
 public class CustomerController {
 
-    @Autowired
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
