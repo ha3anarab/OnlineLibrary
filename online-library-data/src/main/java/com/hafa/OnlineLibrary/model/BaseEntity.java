b46eb6bc77bd8d11f99d5df4fc62c93af644cd20
Ha3aN <Ha3aN.arab@gmail.com>
2022-04-11 17:44:00 +0430

    implement BaseEntity

--- online-library-data/src/main/java/com/hafa/OnlineLibrary/model/BaseEntity.java
+++ online-library-data/src/main/java/com/hafa/OnlineLibrary/model/BaseEntity.java
@@ -3,11 +3,18 @@ package com.hafa.OnlineLibrary.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
