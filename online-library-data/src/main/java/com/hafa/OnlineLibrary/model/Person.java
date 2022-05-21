c92c86dc786c28afebb10e687a7e2107e23f1a70
Ha3aN <Ha3aN.arab@gmail.com>
2022-05-21 11:58:52 +0430

    Jpa mapping for Person class

--- online-library-data/src/main/java/com/hafa/OnlineLibrary/model/Person.java
+++ online-library-data/src/main/java/com/hafa/OnlineLibrary/model/Person.java
@@ -3,10 +3,17 @@ package com.hafa.OnlineLibrary.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 @Getter
 @Setter
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 }
