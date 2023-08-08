140909fa61f936a81f3c6d1cb2b7146107e92b07
ha3anarab <ha3an.arab@gmail.com>
2023-08-08 10:59:47 +0330

    add new Exception class

---
+++ online-library-data/src/main/java/com/hafa/OnlineLibrary/exception/RecordNotFoundException.java
@@ -0,0 +1,25 @@
+package com.hafa.OnlineLibrary.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends RuntimeException {
+
+    private String exceptionDetail;
+    private Object fieldValue;
+
+    public RecordNotFoundException(String exceptionDetail, Long fieldValue) {
+        super(exceptionDetail + " - " + fieldValue);
+        this.exceptionDetail = exceptionDetail;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getExceptionDetail() {
+        return exceptionDetail;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
